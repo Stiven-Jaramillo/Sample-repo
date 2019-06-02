0d162855b8bea245874735a3f44072ef72deb7d5
Stiven Jaramillo <stiven.jaramillo.piza@gmail.com>
2019-06-02 17:49:34 -0500

    New commit

--- S.py
+++ S.py
@@ -1,2 +1,15 @@
-a = 2 * 2
-b = a * 100
+import matplotlib.pylab as plt
+import networkx as nx
+import numpy as np
+
+points_list = [(0, 1), (1, 5), (5, 6), (5, 4), (1, 2), (2, 3), (2, 7)]
+
+goal = 7
+
+G = nx.Graph()
+G.add_edges_from(points_list)
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G, pos)
+nx.draw_networkx_edges(G, pos)
+nx.draw_networkx_labels(G, pos)
+plt.show()
