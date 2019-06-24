6cc62b8a40bf67897264ae6a6701317f668d998d
Stiven Jaramillo <stiven.jaramillo.piza@gmail.com>
2019-06-24 14:57:02 -0500

    New_Stuff

---
+++ face_recog.py
@@ -0,0 +1,20 @@
+import face_recognition
+
+image_of_jim = face_recognition.load_image_file(
+    "./face_recognition/img/known/Jim Carrey.jpg"
+)
+jim_face_encoding = face_recognition.face_encodings(image_of_jim)[0]
+
+unknow_image = face_recognition.load_image_file(
+    "./face_recognition/img/unknown/Dumb.jpg"
+)
+unknown_face_encoding = face_recognition.face_encodings(unknow_image)[0]
+
+# compare faces
+results = face_recognition.compare_faces([jim_face_encoding], unknown_face_encoding)
+
+if results[0]:
+    print("This is Jim Carrey")
+else:
+    print("This is NOT Jim Carrey")
+
