80642318d99d25be5fed4b87d0059a7ce8d6f8f7
Stiven Jaramillo <stiven.jaramillo.piza@gmail.com>
2019-06-24 16:39:48 -0500

    cut this

--- pulling_faces.py
+++ pulling_faces.py
@@ -6,7 +6,6 @@ face_locations = face_recognition.face_locations(image)
 
 for face_location in face_locations:
     top, right, bottom, left = face_locations
-
-face_image = image[top:bottom, left:right]
-pil_image = Image.fromarray(face_image)
-pil_image.show()
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
