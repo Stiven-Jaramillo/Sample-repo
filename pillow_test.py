c47c3bad9f1f0ebb6896d7c041133eee006adca5
Stiven Jaramillo <stiven.jaramillo.piza@gmail.com>
2019-06-25 03:01:30 -0500

    Detection_Done

--- pillow_test.py
+++ pillow_test.py
@@ -1,11 +1,10 @@
 from PIL import Image
 
-im = Image.open("./face_recognition/img/groups/team2.jpg")
 
-x1 = 0
-y1 = 0
-x2 = im.width
-y2 = im.height
-cropped_im = im.crop((x1, y1, x2, y2))
+# im = Image.open("./face_recognition/img/groups/team2.jpg")
+image_name = "./face_recognition/img/groups/team1.jpg"
+im = Image.open(image_name)
+
+cropped_im = im.crop((349, 39, 411, 101))
 cropped_im.show()
 
