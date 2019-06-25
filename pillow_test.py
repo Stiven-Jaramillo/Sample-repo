f08617e7f2e3330f6bf91fae2d8bffe0a335e24e
Stiven Jaramillo <stiven.jaramillo.piza@gmail.com>
2019-06-24 21:43:12 -0500

    PIL

--- pillow_test.py
+++ pillow_test.py
@@ -1,5 +1,11 @@
 from PIL import Image
 
 im = Image.open("./face_recognition/img/groups/team2.jpg")
-im.show()
+
+x1 = 0
+y1 = 0
+x2 = im.width
+y2 = im.height
+cropped_im = im.crop((x1, y1, x2, y2))
+cropped_im.show()
 
