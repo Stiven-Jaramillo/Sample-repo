8b062bc7783cb64420bc7a6e788f81d1eedf850a
Stiven Jaramillo <stiven.jaramillo.piza@gmail.com>
2019-06-25 03:09:01 -0500

    Almost

--- PULL.py
+++ PULL.py
@@ -5,7 +5,10 @@ image = face_recognition.load_image_file("./face_recognition/img/groups/team1.jp
 face_locations = face_recognition.face_locations(image)
 
 for face_location in face_locations:
-    top, right, bottom, left = face_locations
+    top, right, bottom, left = face_location
+
     face_image = image[top:bottom, left:right]
     pil_image = Image.fromarray(face_image)
-    pil_image.show()
+    # pil_image.show()
+    pil_image.save(f"{top}.jpg")
+
