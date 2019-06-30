cbe0bfacb94e86899af543dc870b1a5e1e9fb06f
Stiven Jaramillo <stiven.jaramillo.piza@gmail.com>
2019-06-29 22:00:03 -0500

    Haar cascades working

--- face_recog_match.py
+++ face_recog_match.py
@@ -6,7 +6,7 @@ image_of_jim = face_recognition.load_image_file(
 jim_face_encoding = face_recognition.face_encodings(image_of_jim)[0]
 
 unknow_image = face_recognition.load_image_file(
-    "./face_recognition/img/unknown/Dumb.jpg"
+    "face_recognition//img//unknown//Dumb.jpg"
 )
 unknown_face_encoding = face_recognition.face_encodings(unknow_image)[0]
 
--- opencv_first.py
+++ opencv_first.py
@@ -1,13 +1,27 @@
+import numpy as np
 import cv2
+
+face_cascade = cv2.CascadeClassifier("cascades//data//haarcascade_frontalface_alt2.xml")
 
 cap = cv2.VideoCapture(0)
 
-# Para capturar frame por frame
-while True:
-    ret, frame = cap.read()
-    cv2.imshow("stiv_camera", frame)
 
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+while True:
+    # Para capturar frame por frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        print(x, y, w, h)
+        # The region of interes it's the square made of h * w
+        roi_gray = gray[y : y + h, x : x + w]
+        img_item = "my_image.png"
+        # Para guardar una imagen en un archivo especificado (nombre y formato,arreglo numpy)
+        cv2.imwrite(img_item, roi_gray)
+
+    # Para mostrar los frames resultantes
+    cv2.imshow("stiv_camera", frame)
+    if cv2.waitKey(20) & 0xFF == ord("q"):
         break
 
 cap.release()
