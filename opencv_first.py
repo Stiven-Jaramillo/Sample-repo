0fc8d62b163e3946bf5cc74a9726d02a8c06399c
Stiven Jaramillo <stiven.jaramillo.piza@gmail.com>
2019-07-06 11:50:33 -0500

    Reviewed

--- opencv_first.py
+++ opencv_first.py
@@ -15,9 +15,19 @@ while True:
         print(x, y, w, h)
         # The region of interes it's the square made of h * w
         roi_gray = gray[y : y + h, x : x + w]
+        roi_frame = frame[y : y + h, x : x + w]
+
+        # Recognized? deep learning model predict keras, tensorflow, pytorch, scikit learn
+
         img_item = "my_image.png"
         # Para guardar una imagen en un archivo especificado (nombre y formato,arreglo numpy)
         cv2.imwrite(img_item, roi_gray)
+
+        color = (51, 255, 255)  # BGR 0-255 and this time its blue
+        stroke = 2  # How thick ists the line of the rectangle
+        end_coord_x = x + w
+        end_coord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y), color, stroke)
 
     # Para mostrar los frames resultantes
     cv2.imshow("stiv_camera", frame)
