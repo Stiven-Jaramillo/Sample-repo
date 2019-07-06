b41e4eb53977a06dcb3be125662b2fd5e54e6732
Stiven Jaramillo <stiven.jaramillo.piza@gmail.com>
2019-07-06 16:10:09 -0500

    Reviewed_net

---
+++ simul_clase09.py
@@ -0,0 +1,6 @@
+from openpyxl import load_workbook
+
+wb2 = load_workbook(
+    "C://Users//Lenovo//Desktop//Stiv//UIS online//Simulación-S//Clases//1 - Clase #9 - Simulación.xlsm"
+)
+print(wb2.sheetnames)
